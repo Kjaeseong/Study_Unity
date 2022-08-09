d6743dcc297347eec1b6acaf20cd1a13919e6342
Kjaeseong <103081763+Kjaeseong@users.noreply.github.com>
2022-08-09 17:33:55 +0900

    220808 수업내용 반영

--- 3D/220808_Study_UI/Assets/UIMgr.cs
+++ 3D/220808_Study_UI/Assets/UIMgr.cs
@@ -4,15 +4,77 @@ using UnityEngine;
 
 public class UIMgr : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+    enum PlayerClass
+    { 
+        WARRIOR,
+        SKELETON,
+        CAT
     }
+    
+    public GameObject Warrior;
+    public GameObject Skeleton;
+    public GameObject Cat;
 
-    // Update is called once per frame
+    public int SelectClass = 0;
+
     void Update()
     {
-        
+        ActiveClass();
+        RotateClass();
     }
+
+    void ActiveClass()
+    {
+        if (SelectClass == (int)PlayerClass.WARRIOR)
+        {
+            Warrior.SetActive(true);
+        }
+        else
+        {
+            Warrior.SetActive(false);
+        }
+        if (SelectClass == (int)PlayerClass.SKELETON)
+        {
+            Skeleton.SetActive(true);
+        }
+        else
+        {
+            Skeleton.SetActive(false);
+        }
+        if (SelectClass == (int)PlayerClass.CAT)
+        {
+            Cat.SetActive(true);
+        }
+        else
+        {
+            Cat.SetActive(false);
+        }
+
+    }
+
+    void RotateClass()
+    {
+        if (SelectClass == (int)PlayerClass.WARRIOR)
+        {
+            Skeleton.transform.Rotate(Vector3.up * Time.deltaTime * 20);
+            Skeleton.transform.rotation = Quaternion.Euler(0f, 180f, 0f);
+            Cat.transform.rotation = Quaternion.Euler(0f, 180f, 0f);
+        }
+        else if (SelectClass == (int)PlayerClass.SKELETON)
+        {
+
+            Warrior.transform.rotation = Quaternion.Euler(0f, 180f, 0f);
+            Cat.transform.rotation = Quaternion.Euler(0f, 180f, 0f);
+        }
+        else if (SelectClass == (int)PlayerClass.CAT)
+        {
+
+            Skeleton.transform.rotation = Quaternion.Euler(0f, 180f, 0f);
+            Warrior.transform.rotation = Quaternion.Euler(0f, 180f, 0f);
+        }
+
+    }
+
+    
+
 }
